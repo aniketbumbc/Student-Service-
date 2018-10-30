c165227271cc1f5f486c2aa45625565c67f3deaa
aniket umbc <aniket.umbc@gmail.com>
2018-10-30 08:19:57 +0530

    Added Service and Save Method

---
+++ StudentService/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentService
+{
+    public class Student
+    {
+        private int _id;
+        private string _name;
+        private string _gender;
+        private DateTime _dateofbirth;
+        private string _city;
+        private string _school;
+        private int _fees;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+        public DateTime BirthDate
+        {
+            get { return _dateofbirth; }
+            set { _dateofbirth = value; }
+        }
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+        public string School
+        {
+            get { return _school; }
+            set { _school = value; }
+        }
+        public int Fees
+        {
+            get { return _fees; }
+            set { _fees = value; }
+        }
+    }
+}
---
+++ StudentService/StudentService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace StudentService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "StudentService" in both code and config file together.
+    public class StudentService : IStudentService
+    {
+        public void DeleteStudent(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudent(int Id)
+        {
+            Student student = new Student();
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter parameterID = new SqlParameter();
+                parameterID.ParameterName = "@Id";
+                parameterID.Value = Id;
+                cmd.Parameters.Add(parameterID);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.Name = reader["Name"].ToString();
+                    student.Gender = reader["Gender"].ToString();
+                    student.BirthDate = Convert.ToDateTime(reader["DateOfBirth"]);
+                    student.Fees = Convert.ToInt32(reader["Fees"]);
+                    student.City = reader["City"].ToString();
+                    student.School = reader["School"].ToString();
+                }
+                return student;
+            }
+
+        }
+
+        public void SaveStudent(Student student)
+        {
+            
+        }
+    }
+}
